b67bbaa40a506a16a188b02c6779041f78ac7c43
Yuka Yamamoto <yamamoto_yuka@hotmail.com>
2022-05-07 10:50:23 -0700

    added new category feature

--- js/category.js
+++ js/category.js
@@ -1,13 +1,18 @@
-$(".categoty-wrapper span").click(function () {
-  let value = $(this).attr("data-category");
-  if (value === "all") {
-    $(".news-wrapper").show("1000");
-  } else {
-    $(".news-wrapper")
-      .not("." + value)
-      .hide("1000");
-    $(".news-wrapper")
-      .filter("." + value)
-      .show("1000");
-  }
+$(document).on("click", ".category-items", function () {
+  $(".category-items").removeClass("active-category");
+  $(this).addClass("active-category");
 });
+
+// $(".categoty-wrapper span").click(function () {
+//   let value = $(this).attr("data-category");
+//   if (value === "all") {
+//     $(".news-wrapper").show("1000");
+//   } else {
+//     $(".news-wrapper")
+//       .not("." + value)
+//       .hide("1000");
+//     $(".news-wrapper")
+//       .filter("." + value)
+//       .show("1000");
+//   }
+// });
